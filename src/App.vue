749461592aae1dc4d5eea866e787b8e154e3b777
Giuliano Blas <giuliano.blass@gmail.com>
2023-10-18 10:57:59 +0200

    create : template project

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script >
+  import headerApp   from './components/HeaderApp.vue';
+
+  import mainApp  from './components/MainApp.vue';
+
+  import footerApp  from './components/FooterApp.vue';
+
+  export default {
+
+    components:{
+
+      headerApp,
+
+      mainApp,
+
+      footerApp
+
+    },
+
+    data(){
+
+      return {}
+
+    },
+
+    methods:{
+
+      
+    }
+    
+  }
+
+ </script>
+
+ <template>
+
+    <headerApp/>
+
+    <mainApp/>
+
+    <footerApp/>
+
+ </template>
+
+ <style lang="scss">
+
+  *{
+
+    margin : 0;
+
+    padding: 0;
+
+    box-sizing: border-box;
+
+  }
+
+ </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,36 @@
+<script >
+  
+  export default {
+
+    components:{
+
+      
+
+    },
+
+    data(){
+
+      return {}
+
+    },
+
+    methods:{
+
+      
+    }
+    
+  }
+
+ </script>
+
+ <template>
+
+   header
+
+ </template>
+
+ <style lang="scss" scoped>
+
+  
+
+ </style>
